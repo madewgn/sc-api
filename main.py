a1ddbd5b7953b0a1abe083419fe491f64774e6b8
madewgn <madewg.n2@gmail.com>
2024-01-03 22:13:36 +0000

    x

--- main.py
+++ main.py
@@ -47,5 +47,5 @@ async def vl(u,pw,exp):
     return create.vless(u,pw,exp)
 
 if __name__ == "__main__":
-   uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
+   uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
